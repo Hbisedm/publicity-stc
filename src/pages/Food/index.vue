bfa854ecf68756ff077a34ee76125228d690f899
Chen_QXuan <25414951@qq.com>
2023-02-15 23:41:49 +0800

    ✨ feat:美食页面 布局

--- src/pages/Food/index.vue
+++ src/pages/Food/index.vue
@@ -1,13 +1,90 @@
 <script setup lang="ts">
-
+import Shuffling from '../../components/shuffling.vue'
+import { ref, onMounted } from "vue";
+const topBox = ref()
+const h = ref<number>(0)
+onMounted(() => {
+  h.value = topBox.value.offsetHeight
+})
 </script>
 
 <template>
-  <div>
-    food
+  <div id="box">
+    <div class="topBox" ref="topBox">
+      <div class="top-img-box">
+        <Shuffling :height="`${h}px`"></Shuffling>
+      </div>
+
+      <div class="top-text-box">
+
+      </div>
+    </div>
+
+    <div class="bottomBox">
+
+      <div class="bottom-text-box">
+
+      </div>
+      <div class="bottom-img-box">
+
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped >
+#box {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: calc(100vh - 96px);
+  background-image: linear-gradient(#e66465, #9198e5);
+}
 
+
+
+// 顶部 begin
+.topBox {
+  height: 45%;
+  display: flex;
+  background-color: red;
+}
+
+.top-img-box {
+  width: 50%;
+}
+
+.top-text-box {
+  width: 50%;
+  background-color: yellow;
+}
+// 顶部 end
+
+
+
+
+
+
+
+// 底部 begin
+.bottomBox {
+  height: 45%;
+  display: flex;
+
+  background-color: chartreuse;
+}
+
+.bottom-text-box {
+  width: 45%;
+  background-color: yellow;
+
+}
+
+.bottom-img-box {
+  width: 55%;
+  background-color: chartreuse;
+
+}
+
+// 底部 end
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import Unocss from 'unocss/vite'
 import { presetAttributify, presetIcons, presetUno } from 'unocss'
 import postcssPresetEnv from 'postcss-preset-env'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +14,12 @@ export default defineConfig({
     vue(),
     Unocss({
       presets: [presetUno(), presetAttributify(), presetIcons()],
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
     }),
   ],
   resolve: {
