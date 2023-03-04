38e17d5771a1ad6769f55d4a346f5583a90f7727
Chen-QXuan <25414951@qq.com>
2023-03-04 20:56:36 +0800

    ✨ feat:新增封面接口

---
+++ src/dictionary/surfacePlot.ts
@@ -0,0 +1,29 @@
+import type { ISubject } from '@/interface'
+
+export const coverTopic: ISubject[] =  [
+    {
+        label: '123',
+        subhead: '粤东明珠',
+        enterName: '走进汕头',
+        tag: ['海滨邹鲁','岭东门户','华南要冲', '红色故都'],
+    },
+    {
+        label: '123',
+        subhead: '经济特区',
+        enterName: '123',
+        tag: ['金三角','海上明珠', '工业立市','产业强市', '东方莫斯科'],
+    },
+    {
+        label: 'Welcome',
+        subhead: '百载商埠',
+        enterName: '进入专题',
+        tag: ['海上丝绸','楼船万国','和美侨乡'],
+    },
+    {
+        label: '123',
+        subhead: '美食之都',
+        enterName: '123',
+        tag: [],
+    },
+    
+]
---
+++ src/interface/surfacePlot.ts
@@ -0,0 +1,12 @@
+interface ISubject {
+    // 主标题
+    label: string,
+    // 副标题
+    subhead: string,
+    // 进入专题
+    enterName: string,
+    // 小标签
+    tag: string[]
+}
+
+export type { ISubject }
